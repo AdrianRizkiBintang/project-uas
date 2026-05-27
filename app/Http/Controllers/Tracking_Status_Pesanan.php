a21f39d6e5fdbd94b655aa2fc8dfc3c5d0dfa30d
AbdulGhani <abdulghaniii757@gmail.com>
2026-05-27 23:32:09 +0700

    feat: initialize Laravel application with basic routing, testing, and storage configurations
    - Added console command for inspiring quotes.
    - Created web route for the home page.
    - Configured .gitignore files for storage and framework directories.
    - Implemented basic feature and unit tests.
    - Set up Vite configuration for asset management with Tailwind CSS support.

---
+++ app/Http/Controllers/Tracking_Status_Pesanan.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Tracking_Status_Pesanan extends Controller
+{
+    //
+}
---
+++ app/Models/Live_Tracking_Kurir.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Live_Tracking_Kurir extends Model
+{
+    //
+}
