bf1aa6880529e0acf7e5c01ec8e84b338fc477cb
Khresnanda Putra Wirawan <khresnanda12@gmail.com>
2026-05-28 22:58:35 +0700

    Feat: Pembaruan konfigurasi proyek dan inisialisasi struktur testing

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-<<<<<<< HEAD
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
-=======
-<<<<<<< HEAD
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
-=======
->>>>>>> cd1aac3ff3eb328e01c7ec3a7a1c81eba9d6d37f
-<<<<<<< HEAD
->>>>>>> friend-repo/main
-=======
->>>>>>> 43b70fe15192ffe77de4cb776a49bd82a85fc629
->>>>>>> 3abcfe4cede430b0b2dc1e5e423bb33d0da8224f
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> friend-repo/main
-=======
->>>>>>> 43b70fe15192ffe77de4cb776a49bd82a85fc629
->>>>>>> 3abcfe4cede430b0b2dc1e5e423bb33d0da8224f
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
             ],
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
-<<<<<<< HEAD
-=======
-=======
+            refresh: true,
         }),
     ],
->>>>>>> cd1aac3ff3eb328e01c7ec3a7a1c81eba9d6d37f
-<<<<<<< HEAD
->>>>>>> friend-repo/main
-=======
->>>>>>> 43b70fe15192ffe77de4cb776a49bd82a85fc629
->>>>>>> 3abcfe4cede430b0b2dc1e5e423bb33d0da8224f
 });
